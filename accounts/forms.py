21ca780b4fd7bfb8524824023ab00486a71765d1
Sparsh Kumar <S.Kumar10@ncl.ac.uk>
2024-10-08 16:25:59 +0100

    Added a form for the users to register. Includes fields for the registration form.

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from setuptools.config.pyprojecttoml import validate
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+class RegistrationForm(FlaskForm):
+    email = StringField(validators = [DataRequired()])
+    firstname = StringField(validators = [DataRequired()])
+    lastname = StringField(validators = [DataRequired()])
+    phone = StringField(validators = [DataRequired()])
+    password = PasswordField(validators = [DataRequired()])
+    confirm_password = PasswordField(validators = [DataRequired(), EqualTo('password', message = "Both Password fields must be equal")])
+    submit = SubmitField
+
